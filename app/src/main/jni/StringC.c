ab0a1e39d480c5b388f302fd55ab1e7b77835c08
machao <359423168@qq.com>
2017-08-23 17:32:16 +0800

    提交

--- app/src/main/jni/StringC.c
+++ app/src/main/jni/StringC.c
@@ -1,6 +1,4 @@
 #include "com_example_machao_myndksecrecy_Secrety.h"
-
-
 JNIEXPORT jstring JNICALL  Java_com_example_machao_myndksecrecy_Secrety_getSecrety(JNIEnv *env, jobject thiz)  {
     return  (*env)->NewStringUTF(env, "www.baidu.com");;
 }
